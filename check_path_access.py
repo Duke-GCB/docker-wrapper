943b3a40667d8dd27547c13c4d5ac9fe00d86e03
Dan Leehr <dan.leehr@duke.edu>
2015-06-23 10:48:38 -0400

    Add positional arg specifiers for compat w/ 2.6

--- check_path_access.py
+++ check_path_access.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os
 import sys
 from parse_docker_args import parse_mount
@@ -16,15 +14,15 @@ def can_access(path, perm):
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-    print "USAGE: {} <docker volume spec>".format(sys.argv[0])
-    print "e.g. {} /data/somelab:/input:rw".format(sys.argv[0])
+    print "USAGE: {0} <docker volume spec>".format(sys.argv[0])
+    print "e.g. {0} /data/somelab:/input:rw".format(sys.argv[0])
     exit(1)
   volume_spec = sys.argv[1]
   path, perm = parse_mount(volume_spec)
   uid = os.getuid()
   if can_access(path, perm):
-    print "PASS: UID {} has {} access to {}".format(uid, perm, path)
+    print "PASS: UID {0} has {1} access to {2}".format(uid, perm, path)
     exit(0)
   else:
-    print "ERROR: UID {} has no {} access to {}".format(uid, perm, path)
+    print "ERROR: UID {0} has no {1} access to {2}".format(uid, perm, path)
     exit(1)
--- check_path_whitelist.py
+++ check_path_whitelist.py
@@ -7,7 +7,7 @@ PERMITTED_PATHS = ('/gpfs/fs0/data/',) # Trailing / is important so that /datafi
 def check_permitted_path(path):
   # Paths must resolve to something that starts with a PERMITTED_PATH
   resolved = os.path.realpath(path)
-  print 'Resolved path to {}'.format(resolved)
+  print 'Resolved path to {0}'.format(resolved)
   valid = False
   for permitted in PERMITTED_PATHS:
     if resolved.startswith(permitted): valid = True
@@ -15,14 +15,14 @@ def check_permitted_path(path):
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-    print "USAGE: {} <docker volume spec>".format(sys.argv[0])
-    print "e.g. {} /data/somelab:/input:rw".format(sys.argv[0])
+    print "USAGE: {0} <docker volume spec>".format(sys.argv[0])
+    print "e.g. {0} /data/somelab:/input:rw".format(sys.argv[0])
     exit(1)
   volume_spec = sys.argv[1]
   path, perm = parse_mount(volume_spec)
   if check_permitted_path(path):
-    print "PASS: {} mount permitted".format(path)
+    print "PASS: {0} mount permitted".format(path)
     exit(0)
   else:
-    print "ERROR: {} mount is not permitted".format(path)
+    print "ERROR: {0} mount is not permitted".format(path)
     exit(1)
