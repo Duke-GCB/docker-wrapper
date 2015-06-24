7a13286aaf135bcd1b67c750ad6d70729fdeeaba
Dan Leehr <dan.leehr@duke.edu>
2015-06-24 15:34:00 -0400

    Add setup.py for distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='docker-wrapper',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/Duke-GCB/docker-wrapper',
+    license='MIT',
+    author='dcl9',
+    author_email='dan.leehr@duke.edu',
+    description='Checks filesystem access before mounting docker volumes'
+)
