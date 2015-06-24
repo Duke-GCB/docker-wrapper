b5b0e719f7f2a1550ff1f601a2ce25412d5b3df6
Dan Leehr <dan.leehr@duke.edu>
2015-06-24 15:11:29 -0400

    Use absolute path to python bin

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -11,19 +11,22 @@ fi
 # Get the directory of this script, for paths to python files
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Specify exact python location - uses 2.7 and doesn't rely on path
+PYTHON_BIN="/usr/local/bin/python"
+
 # Pick out the volumes
 VOLUME_ARG=0
 for i in $*
 do
   if [ $VOLUME_ARG -eq 1 ]; then
     # Check access to volume
-    ACCESS=$(sudo -u "#$SUDO_UID" python "$DIR/check_path_access.py" $i)
+    ACCESS=$(sudo -u "#$SUDO_UID" "$PYTHON_BIN" "$DIR/check_path_access.py" $i)
     if [ $? -ne 0 ]; then
       echo $ACCESS
       exit 1
     fi
     # Ensure path is whitelisted
-    IS_DATA=$(sudo -u "#$SUDO_UID" python "$DIR/check_path_whitelist.py" $i)
+    IS_DATA=$(sudo -u "#$SUDO_UID" "$PYTHON_BIN" "$DIR/check_path_whitelist.py" $i)
     if [ $? -ne 0 ]; then
       echo $IS_DATA
       exit 1
